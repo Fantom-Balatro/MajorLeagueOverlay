8c07440d81480db0cbbfc6f4daae5207138777aa
davidmolgard <molgarddavid@gmail.com>
2025-07-15 22:47:50 -0600

    finished v1.0

--- Main.lua
+++ Main.lua
@@ -18,7 +18,7 @@ local buy_from_shop_ref = G.FUNCS.buy_from_shop
 function G.FUNCS.buy_from_shop(e)
 	local c1 = e.config.ref_table
 	if c1 and c1:is(Card) then
-		MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "bought_card", cost = e.cost, card = c1.ability.name})
+		MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "bought_card", card = c1.config.center_key})
     end
     return buy_from_shop_ref(e)
 end
@@ -27,6 +27,9 @@ local use_card_ref = G.FUNCS.use_card
 function G.FUNCS.use_card(e, mute, nosave)
 	if e.config and e.config.ref_table and e.config.ref_table.shop_voucher and e.config.ref_table.config and e.config.ref_table.config.center_key then
 		MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "bought_voucher",  new_voucher = e.config.ref_table.config.center_key, vouchers = MO.UTILS.get_vouchers()})
+	end
+	if e.config and e.config.ref_table and not e.config.ref_table.shop_voucher and e.config.ref_table.config and e.config.ref_table.config.center.atlas ~= "Booster" and e.config.ref_table.config.center_key then
+		MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "bought_card",  card = e.config.ref_table.config.center_key})
 	end
 	return use_card_ref(e, mute, nosave)
 end
@@ -41,4 +44,28 @@ local ease_ante_ref = ease_ante
 function ease_ante(mod, instant)
     MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "ante_reached", amount = G.GAME.round_resets.ante + mod})
     return ease_ante_ref(mod, instant)
+end
+
+local ease_lives_ref = ease_lives
+function ease_lives(mod, instant)
+	MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "life_lost"})
+	return ease_lives_ref(mod, instant)
+end
+
+local start_run_ref = Game.start_run
+function Game:start_run(args)
+	if MP and MP.LOBBY then
+		MO.pvpScore = 0
+		MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "game_start", starting_lives = MP.LOBBY.config.starting_lives})
+	end
+	return start_run_ref(self, args)
+end
+
+local play_hand_ref = MP.ACTIONS.play_hand
+function MP.ACTIONS.play_hand(score, hands_left)
+	if score > 0 then
+		MO.UTILS.send_json_event(MO.serverUrl, {user = MP.UTILS.get_username(), action = "play_hand", score = score - MO.pvpScore, hands_left = hands_left})
+	end
+	MO.pvpScore = score
+	return play_hand_ref(score, hands_left)
 end
